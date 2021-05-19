94957f6359d825c86370b137851ddd5db3d363bc
Felipe <Felipe@DESKTOP-QOE7IOD>
2021-05-18 23:37:19 -0500

    Agregada y cambiada la estructura singlenton

--- src/abccsqlite/Conexion/ConexionBase.java
+++ src/abccsqlite/Conexion/ConexionBase.java
@@ -9,21 +9,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBase {
-    public static Connection con; 
+
+    private static Connection con;
+
     public static Connection getconn() {
-    if (con == null){
-        try {
-   
-      Class.forName("org.sqlite.JDBC");
-      con = DriverManager.getConnection("jdbc:sqlite:C:/Users/Felipe/Documents/NetBeansProjects/AbccSqlit/src/BaseDatos.db"); // connecting to our database
-      System.out.println("Connected!");
-    } catch (ClassNotFoundException | SQLException e ) {
-     
-      System.out.println(e+"");
+        if (con == null) {
+            try {
+
+                Class.forName("org.sqlite.JDBC");
+                con = DriverManager.getConnection("jdbc:sqlite:C:/Users/Felipe/Documents/NetBeansProjects/AbccSqlit/src/BaseDatos.db"); // connecting to our database
+                System.out.println("Connected!");
+            } catch (ClassNotFoundException | SQLException e) {
+
+                System.out.println(e + "");
+            }
+        }
+
+        return con;
     }
-    }
-    
-    
-    return con; 
-  }
 }
