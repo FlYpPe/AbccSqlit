edce53e23c1c8e5d993b692ed0f680ad4c8d1bab
Felipe <Felipe@DESKTOP-QOE7IOD>
2021-05-20 00:09:40 -0500

    añadido metodo con prepared statement para agregar registros

--- src/DAO/Controlador.java
+++ src/DAO/Controlador.java
@@ -7,6 +7,8 @@ package DAO;
 
 import abccsqlite.Conexion.ConexionBase;
 import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,21 +19,36 @@ import java.sql.SQLException;
  */
 public class Controlador {
 
-    public static void agregarRegistro(String idOrden, String fecha, String cantidad, String tipoPago, String idMesero) throws SQLException {
-
-        
+    public static void agregarRegistro(String sql) throws SQLException {
 
     }
 
-    public static void modificarRegistro(String idOrden, String fecha, String cantidad, String tipoPago, String idMesero) throws SQLException {
+    public static void insertarCliente(String fName, String lName, String telNo, String prefType, String maxRent) {
+        Connection con = ConexionBase.getconn();
 
-        
+        PreparedStatement ps = null;
+        try {
+            String sql = "INSERT INTO client(fName, lName, telNo, prefType, maxRent) VALUES(?,?,?,?,?) ";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, fName);
+            ps.setString(2, lName);
+            ps.setString(3, telNo);
+            ps.setString(4, prefType);
+            ps.setInt(5, Integer.parseInt(maxRent));
+            ps.execute();
+            System.out.println("Data has been inserted!");
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            // always remember to close database connections
+        } 
+    }
+
+    public static void modificarRegistro(String idOrden, String fecha, String cantidad, String tipoPago, String idMesero) throws SQLException {
 
     }
 
     public static void eliminarRegistro(String idOrden) throws SQLException {
 
-        
     }
 
     public static ResultSet buscarRegistro(String sql) throws SQLException {
@@ -46,20 +63,18 @@ public class Controlador {
 
         PreparedStatement ps = ConexionBase.getconn().prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
-        String pass = "";        
+        String pass = "";
         while (rs.next()) {
             String user = rs.getString("user");
             pass = rs.getString("pass");
 
             System.out.println("user: " + user);
             System.out.println("pass: " + pass);
-           
-            
 
         }
         System.out.println(pass);
         System.out.println(passw);
-        if(passw.equals(pass)){
+        if (passw.equals(pass)) {
             System.out.println("qqqqq");
             return true;
         }
