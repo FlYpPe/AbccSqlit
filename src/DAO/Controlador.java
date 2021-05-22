60ff28021c38e66d118bff6af77a4d183d37c0ea
Felipe <Felipe@DESKTOP-QOE7IOD>
2021-05-21 23:09:56 -0500

    Añadido metodos con ps para borrar y editar

--- src/DAO/Controlador.java
+++ src/DAO/Controlador.java
@@ -23,6 +23,43 @@ public class Controlador {
 
     }
 
+    public static void editarCliente(String fName, String lName, String telNo, String prefType, String maxRent, String clientNo) {
+        Connection con = ConexionBase.getconn();
+        PreparedStatement ps = null;
+        try {
+            String sql = "UPDATE Client set fName = ?, lName = ?, telNo = ?, prefType = ?, maxRent = ?  WHERE clientNo = ? ";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, fName);
+            ps.setString(2, lName);
+            ps.setString(3, telNo);
+            ps.setString(4, prefType);
+            ps.setInt(5, Integer.parseInt(maxRent));
+            ps.setInt(6, Integer.parseInt(clientNo));
+            ps.execute();
+            System.out.println("Data has been updated");
+        } catch (SQLException e) {
+            // TODO: handle exception
+            System.out.println(e.toString());
+        }
+    }
+    
+    public static void borrarCliente(String clientNo){
+        Connection con = ConexionBase.getconn();
+        PreparedStatement ps = null;
+        try {
+            String sql = "DELETE FROM Client where clientNo = ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, Integer.parseInt(clientNo));
+            ps.execute();
+            System.out.println("Data has been inserted!");
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            // always remember to close database connections
+        }
+        
+        
+    }
+
     public static void insertarCliente(String fName, String lName, String telNo, String prefType, String maxRent) {
         Connection con = ConexionBase.getconn();
 
@@ -40,7 +77,7 @@ public class Controlador {
         } catch (SQLException e) {
             System.out.println(e.toString());
             // always remember to close database connections
-        } 
+        }
     }
 
     public static void modificarRegistro(String idOrden, String fecha, String cantidad, String tipoPago, String idMesero) throws SQLException {
